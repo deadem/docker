4f57cd2875f01ed83d3063ce1944a074bdaebb0c
deadem <deadem@gmail.com>
2022-09-25 23:07:09 +0300

    fix line endings

--- clone.sh
+++ clone.sh
@@ -1,17 +1,17 @@
-cwd=$(pwd)
-mkdir $cwd/repo
-
-mkdir /tmp/repo >/dev/null 2>&1
-cd /tmp/repo
-
-git clone $(perl -e "\$x=\"$1\"; \$x=~s/\/pull\/.+//; print \$x;") .
-git fetch origin $(perl -e "\$x=\"$1\"; \$x=~s/.*?\/(pull\/.+)/\$1\/head:merge_request/; print \$x;") && git checkout merge_request
-
-cp -R . $cwd/repo
-npm install
-
-/bin/bash
-rm -rf $cwd/repo
-rm -rf /tmp/repo
-
+cwd=$(pwd) #
+mkdir $cwd/repo #
+#
+mkdir /tmp/repo >/dev/null 2>&1 #
+cd /tmp/repo #
+#
+git clone $(perl -e "\$x=\"$1\"; \$x=~s/\/pull\/.+//; print \$x;") . #
+git fetch origin $(perl -e "\$x=\"$1\"; \$x=~s/.*?\/(pull\/.+)/\$1\/head:merge_request/; print \$x;") && git checkout merge_request #
+#
+cp -R . $cwd/repo #
+npm install #
+#
+/bin/bash #
+rm -rf $cwd/repo #
+rm -rf /tmp/repo #
+#
 #using: clone https://github.com/putiniyama/middle.messenger.praktikum.yandex/pull/1
